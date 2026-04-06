c94fc698c84332f09c4dbf92069619caf4a83d31
Jisshnu H J <jisshnu2003@gmail.com>
2026-04-06 19:57:14 +1000

    Initial React Setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Skills />
+      <Extracurricular />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
